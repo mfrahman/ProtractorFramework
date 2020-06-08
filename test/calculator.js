0ba1b695cd32ed3de9cda3216922120715122773
mfrahman <rahmanfayez1@gmail.com>
2020-06-08 15:45:40 -0400

    updated calculator.js

--- test/calculator.js
+++ test/calculator.js
@@ -20,5 +20,23 @@ describe('demo calculator tests', function () {
 
   });
 
+  it('subtraction test', function () {
+
+    homepage.get('http://juliemr.github.io/protractor-demo/');
+
+    homepage.enterFirstNumber(4);
+    homepage.enterSecondNumber(3);
+    homepage.clikGo1();
+
+    homepage.verifyResult('7');
+
+    browser.sleep(3000);
+
+
+
+
+
+  });
+
 
 });
