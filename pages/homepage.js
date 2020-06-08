4874ecf9cefe454b44d0028e261de4dfb0144c18
mfrahman <rahmanfayez1@gmail.com>
2020-06-07 23:07:51 -0400

    My first project commit

---
+++ pages/homepage.js
@@ -0,0 +1,34 @@
+let homepage = function () {
+
+    let firstNumber_input = element(by.model('first'));
+    let secondNumber_input = element(by.model('second'));
+    let goButton = element(by.css('[ng-click="doAddition()"]'));
+
+    this.get = function (url) {
+        browser.get(url)
+    }
+
+    this.enterFirstNumber = function (firstNo) {
+        firstNumber_input.sendKeys(firstNo);
+
+    }
+    this.enterSecondNumber = function (SecondNo) {
+        secondNumber_input.sendKeys(SecondNo);
+
+    }
+
+    this.clikGo = function () {
+        goButton.click();
+
+    }
+
+    this.verifyResult = function (result) {
+        let output = element(by.cssContainingText('.ng-binding', result));
+
+        expect(output.getText()).toEqual(result);
+    }
+
+
+};
+module.exports = new homepage();
+
---
+++ test/calculator.js
@@ -0,0 +1,24 @@
+let homepage = require('../pages/homepage');
+
+describe('demo calculator tests', function () {
+
+  it('addition test', function () {
+
+    homepage.get('http://juliemr.github.io/protractor-demo/');
+
+    homepage.enterFirstNumber(4);
+    homepage.enterSecondNumber(3);
+    homepage.clikGo();
+
+    homepage.verifyResult('7');
+
+    browser.sleep(2000);
+
+
+
+
+
+  });
+
+
+});
